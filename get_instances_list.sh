74eee01769c6f0035fc6519ded0555db61836453
mostafaEmad123456789 <mostafa5mes@gmail.com>
2022-09-03 17:17:16 +0200

    CloudFormation Template

--- get_instances_list.sh
+++ get_instances_list.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-privatekey="/home/mostafa/ansible/project3.pem"
 
+echo "[web]" > inventory.txt
 
-echo "[web]" > /home/mostafa/ansible/inventory.txt
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress]'  --filters "Name=tag:project,Values=section4" --output text >> inventory.txt
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress]'  --filters "Name=tag:project,Values=section4" --output text >> /home/mostafa/ansible/inventory.txt
-
-cat /home/mostafa/ansible/inventory.txt
+cat inventory.txt
